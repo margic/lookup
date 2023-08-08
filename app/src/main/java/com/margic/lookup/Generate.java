b76b3d3baa52df5898b573de859431607251685c
Paul <pmcrofts@margic.com>
2023-08-08 10:17:49 -0700

    clean up

--- app/src/main/java/com/margic/lookup/Generate.java
+++ app/src/main/java/com/margic/lookup/Generate.java
@@ -1,10 +1,5 @@
 package com.margic.lookup;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
